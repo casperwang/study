98586a64b102053ac7692ebeea9de98d3e68ef69
casperwang <casperwang2014@gmail.com>
2017-02-25 08:58:05 +0800

    123

---
+++ js/canvas.js
@@ -0,0 +1,9 @@
+// var canvas = document.getElementById("citizenU3C1");
+// if (canvas.getContext) {
+// 	var ctx = canvas.getContext("2d");
+// 	var img = new Image();
+// 	img.onload = function(){
+// 		ctx.drawImage(img, 0, 0, 300, 261);
+// 	};
+// 	img.src = 'img/citizenU3I1.png';
+// }
